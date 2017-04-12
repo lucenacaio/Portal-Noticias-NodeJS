c3a204932173fad9e7a05bde5c3e0634551fb4d0
Caio De Albuquerque Lucena Medeiros <camedeiros1@stefanini.com>
2017-04-12 08:52:53 -0300

    Finalização do projeto 'Portal de Notícias'

--- app/controllers/noticias.js
+++ app/controllers/noticias.js
@@ -10,9 +10,9 @@ module.exports.noticias = function(application, req, res) {
 module.exports.noticia = function(application, req, res) {
     let connection = application.config.dbConnection();
     let noticiasModel = new application.app.models.NoticiasDAO(connection);
-
-    noticiasModel.getNoticia(function(error, result) {
-        res.render("noticias/noticia", { noticias: result });
+    let id_noticia = req.query;
+    noticiasModel.getNoticia(id_noticia, function(error, result) {
+        res.render("noticias/noticia", { noticia: result });
     });
 
 }
